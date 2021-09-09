30e414fea2ff6bd7bb5a02a61318fa9b94e68955
SHIHONGHAO <13820618441@163.com>
2021-09-09 20:54:19 +0800

    init basic parameters

---
+++ parameters/ArrayData.py
@@ -0,0 +1,19 @@
+from datetime import date
+
+
+class ArrayData:
+    length = 0
+    start_date = None
+    area_zh = None
+    area_en = None
+    data = []
+
+    def __init__(self, data: list, start_date: date = None, area_zh: str = None, area_en: str = None):
+        self.data = data
+        if start_date is not None:
+            self.start_date = start_date
+        if area_zh is not None:
+            self.area_zh = area_zh
+        if area_en is not None:
+            self.area_en = area_en
+        self.length = len(data)

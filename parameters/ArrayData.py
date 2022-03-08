a057a8ffb01e3bc129b6150a27e42f1e5fe3409d
SHIHONGHAO <13820618441@163.com>
2022-03-08 17:47:16 +0800

    add country_db dataset

--- parameters/ArrayData.py
+++ parameters/ArrayData.py
@@ -6,10 +6,11 @@ class ArrayData:
     start_date = None
     area_zh = None
     area_en = None
-    area_iso3=None
+    area_iso3 = None
     data = []
 
-    def __init__(self, data: list, start_date: datetime.date = None, area_zh: str = None, area_en: str = None,area_iso3:str=None):
+    def __init__(self, data: list, start_date: datetime.date = None, area_zh: str = None, area_en: str = None,
+                 area_iso3: str = None):
         self.data = data
         if start_date is not None:
             self.start_date = start_date
--- parameters/ProfileData.py
+++ parameters/ProfileData.py
@@ -3,16 +3,13 @@
 class ProfileData:
     area_zh = None
     area_en = None
-    time = None
     feature2value = None
 
 
 
 
-    def __init__(self, data: dict, time: str,  area_zh: str = None, area_en: str = None):
+    def __init__(self, data: dict, area_zh: str = None, area_en: str = None):
         self.feature2value = data
-        if time is not None:
-            self.time = time
         if area_zh is not None:
             self.area_zh = area_zh
         if area_en is not None:

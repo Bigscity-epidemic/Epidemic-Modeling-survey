723f6d0eb3eb45af2b1d71bef617839830d08464
SHIHONGHAO <13820618441@163.com>
2022-04-20 16:08:54 +0800

    upd doc

---
+++ examples/test-copy/hello.py
@@ -0,0 +1,9 @@
+from compartment.Graph import Graph
+from compartment.Model import Model
+from executor.Executor import Executor
+from compartment.Descriptor import vertical_divide
+from compartment.Transfer import init_compartment, set_path_exp, set_path_parameters
+from visual.visual_graph import visual_model
+from visual.visual_model_data import visual_compartment_values
+
+print('Welcome to LibEpidemic!')

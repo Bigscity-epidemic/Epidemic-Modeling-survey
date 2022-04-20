d1b69799150d67bd6cbbf99a156324d3d8d81f9c
SHIHONGHAO <13820618441@163.com>
2022-04-20 16:21:37 +0800

    change alpha

--- examples/test-copy/param.py
+++ examples/test-copy/param.py
@@ -11,14 +11,14 @@ vertical_divide(graph, 'S', ['E', 'I', 'R'])
 model = Model('basic_SEIR', graph)
 
 beta = 0.5
-epsilon = 0.1
+alpha = 0.1
 gamma = 0.1
 population = 10000
 set_path_exp(model, 'S', 'E', 'beta*S*I*popu')
 set_path_parameters(model, 'S', 'E', 'beta', beta)
 set_path_parameters(model, 'S', 'E', 'popu', 1.0 / population)
-set_path_exp(model, 'E', 'I', 'epsilon*E')
-set_path_parameters(model, 'E', 'I', 'epsilon', epsilon)
+set_path_exp(model, 'E', 'I', 'alpha*E')
+set_path_parameters(model, 'E', 'I', 'alpha', alpha)
 set_path_exp(model, 'I', 'R', 'gamma*I')
 set_path_parameters(model, 'I', 'R', 'gamma', gamma)
 

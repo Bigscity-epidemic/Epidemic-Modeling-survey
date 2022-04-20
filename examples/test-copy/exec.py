4a69aa7e8e1200a7810bcf0dfd42b96fe25dd751
SHIHONGHAO <13820618441@163.com>
2022-04-20 16:15:11 +0800

    upd doc

---
+++ examples/test-copy/exec.py
@@ -0,0 +1,30 @@
+from compartment.Graph import Graph
+from compartment.Model import Model
+from executor.Executor import Executor
+from compartment.Descriptor import vertical_divide
+from compartment.Transfer import init_compartment, set_path_exp, set_path_parameters
+from visual.visual_graph import visual_model
+from visual.visual_model_data import visual_compartment_values
+
+graph = Graph('basic_SEIR', 'S')
+vertical_divide(graph, 'S', ['E', 'I', 'R'])
+model = Model('basic_SEIR', graph)
+
+beta = 0.5
+epsilon = 0.1
+gamma = 0.1
+population = 10000
+set_path_exp(model, 'S', 'E', 'beta*S*I*popu')
+set_path_parameters(model, 'S', 'E', 'beta', beta)
+set_path_parameters(model, 'S', 'E', 'popu', 1.0 / population)
+set_path_exp(model, 'E', 'I', 'epsilon*E')
+set_path_parameters(model, 'E', 'I', 'epsilon', epsilon)
+set_path_exp(model, 'I', 'R', 'gamma*I')
+set_path_parameters(model, 'I', 'R', 'gamma', gamma)
+
+init_value = {'S': 9995, 'E': 2, 'I': 3, 'R': 0}
+init_compartment(model, init_value)
+executor = Executor(model)
+for index in range(5):
+    executor.simulate_step(index)
+    visual_compartment_values(model)
